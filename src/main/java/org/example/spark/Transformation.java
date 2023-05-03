5ea37b719f77146580438d3dc33ed9e27549d3c4
Geet Rajendra Patel <geetpofficial@gmail.com>
2023-05-03 14:44:12 -0400

    Init

--- src/main/java/org/example/spark/Transformation.java
+++ src/main/java/org/example/spark/Transformation.java
@@ -1,9 +1,9 @@
-package org.example;
+package org.example.spark;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
-public class Transformations {
+public class Transformation {
     public long count(Dataset<Row> dataset) {
         return dataset.count();
     }
--- src/test/java/org/example/SparkTestSession.java
+++ src/test/java/org/example/SparkTestSession.java
@@ -1,2 +1,18 @@
-package org.example;public class SparkTestSession {
+package org.example;
+
+import org.apache.spark.sql.SparkSession;
+
+public class SparkTestSession {
+    private static SparkSession sparkSession;
+
+    static {
+        sparkSession = SparkSession.builder().appName("sqli-log-analyser-test").master("local[*]").getOrCreate();
+    }
+
+    public static SparkSession get() {
+        if (sparkSession == null) {
+            sparkSession = SparkSession.builder().appName("sqli-log-analyser-test").master("local[*]").getOrCreate();
+        }
+        return sparkSession;
+    }
 }
