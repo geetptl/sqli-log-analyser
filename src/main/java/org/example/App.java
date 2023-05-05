7bd2e59ce75faad01bd0e481908f8deae282c842
Geet Rajendra Patel <geetpofficial@gmail.com>
2023-05-05 17:43:05 -0400

    Basic app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,38 @@
 package org.example;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SparkSession spark = SparkSession.builder().appName("JavaSparkPi").getOrCreate();
+
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+        int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
+        int n = 100 * slices;
+        List<Integer> l = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            l.add(i);
+        }
+
+        JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+        int count = dataSet.map(integer -> {
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            return (x * x + y * y <= 1) ? 1 : 0;
+        }).reduce(Integer::sum);
+
+        System.out.println("Pi is roughly " + 4.0 * count / n);
+
+        spark.stop();
     }
 }
